ad9d5a215a334096aff456fc13e34e23fc6ea8c2
Brian Leach Christensen <blc@elcoyote.dk>
2024-03-22 09:28:52 +0100

    add basic functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -17,6 +18,12 @@ export default ({ mode }: { mode: string }) => {
         },
       },
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        injectRegister: 'auto',
+      }),
+    ],
   });
 };
