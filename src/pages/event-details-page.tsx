34126fd4e134d549804b8140ed29e8bfb6bbe978
Brian Leach Christensen (External) <30732216+blcoyote@users.noreply.github.com>
2024-03-26 08:01:51 +0100

    Update event-details-page.tsx

--- src/pages/event-details-page.tsx
+++ src/pages/event-details-page.tsx
@@ -23,6 +23,7 @@ export const Component = () => {
 			initial={{ opacity: 0, scale: 0.5 }}
 			animate={{ opacity: 1, scale: 1 }}
 			transition={{ duration: 0.5 }}
+			style={{ marginTop: "0.5rem" }}
 		>
 			<IconArrowBackUp onClick={navigateToEventsPage} color="#797979" />
 			<EventContainer
