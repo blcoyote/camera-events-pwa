951658183eb3aeb8e80530c570e30b33e3153fdc
Brian Leach Christensen (External) <30732216+blcoyote@users.noreply.github.com>
2024-04-10 10:18:41 +0200

    Update use-event.tsx

--- src/hooks/use-event.tsx
+++ src/hooks/use-event.tsx
@@ -62,35 +62,33 @@ export const useEventSnapshot = (
 };
 
 export const useEventClipDownload = (id?: string) => {
-	const { api } = useApi();
-	const { token } = useAuthProvider();
-	const { isPending, error, isSuccess, mutate } = useMutation({
-		mutationFn: () =>
-			api
-				.get(`/v2/downloads/${id}/clip.mp4?token=${token}`, {
-					responseType: "arraybuffer",
-				})
-				.then((res) => {
-					fileDownload(res.data, `${id}.mp4`);
-				}),
-	});
+    const { api } = useApi();
+    const { isPending, error, isSuccess, mutate } = useMutation({
+        mutationFn: () =>
+            api
+                .get(`/v2/event/${id}/clip.mp4`, {
+                    responseType: "arraybuffer",
+                })
+                .then((res) => {
+                    fileDownload(res.data, `${id}.mp4`);
+                }),
+    });
 
-	return { loading: isPending, error, isSuccess, download: mutate };
+    return { loading: isPending, error, isSuccess, download: mutate };
 };
 
 export const useEventSnapshotDownload = (id?: string) => {
-	const { api } = useApi();
-	const { token } = useAuthProvider();
-	const { isPending, error, isSuccess, mutate } = useMutation({
-		mutationFn: () =>
-			api
-				.get(`/v2/downloads/${id}/snapshot.jpg?token=${token}`, {
-					responseType: "arraybuffer",
-				})
-				.then((res) => {
-					fileDownload(res.data, `${id}.jpg`);
-				}),
-	});
+    const { api } = useApi();
+    const { isPending, error, isSuccess, mutate } = useMutation({
+        mutationFn: () =>
+            api
+                .get(`/v2/event/${id}/snapshot.jpg`, {
+                    responseType: "arraybuffer",
+                })
+                .then((res) => {
+                    fileDownload(res.data, `${id}.jpg`);
+                }),
+    });
 
-	return { loading: isPending, error, isSuccess, download: mutate };
+    return { loading: isPending, error, isSuccess, download: mutate };
 };
