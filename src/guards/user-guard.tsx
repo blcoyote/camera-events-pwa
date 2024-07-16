8e7b6b4602e387dd726fbfdecf9519d95f6b2ab9
Brian Leach Christensen <30732216+blcoyote@users.noreply.github.com>
2024-07-16 22:18:17 +0200

    add ios checks (#20)
    test
    
    tests

--- src/guards/user-guard.tsx
+++ src/guards/user-guard.tsx
@@ -1,9 +1,28 @@
 import { Flex, Loader } from "@mantine/core";
 import { useAuthProvider } from "../hooks/use-auth-provider";
 import { UnauthenticatedPage } from "../pages/unauthenticated-page";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 export const UserGuard = (props: { children: React.ReactElement }) => {
-	const { user, loading } = useAuthProvider();
+    const { user, loading, autenticated, error } = useAuthProvider();
+
+    useEffect(() => {
+        function ToastDisplay() {
+            return (
+                <div>
+                    <p>
+                        <b>{user?.email}</b>
+                    </p>
+                    <p>{"authenticated:" + autenticated}</p>
+                    <p>{"error:" + error}</p>
+                </div>
+            );
+        }
+        if (!loading) {
+            toast.info(<ToastDisplay />);
+        }
+    }, [user, loading]);
 
     if (loading) {
         return (
--- src/pages/unauthenticated-page.tsx
+++ src/pages/unauthenticated-page.tsx
@@ -2,6 +2,7 @@ export const UnauthenticatedPage = () => {
     return (
         <div>
             <h1>Please log in</h1>
+
         </div>
     );
 };
