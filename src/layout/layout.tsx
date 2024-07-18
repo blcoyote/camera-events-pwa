adb44ad81c458e1182e9082f1b79889c268580f5
Brian Leach Christensen <30732216+blcoyote@users.noreply.github.com>
2024-07-18 22:38:29 +0200

    Update layout.tsx

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -6,7 +6,7 @@ export const Layout = () => {
     return (
         <div className="flex flex-col p-2 min-h-dvh bg-stone-200 ">
             <Navbar />
-            <div className="h-full flex overflow-y-auto flex-col flex-grow ">
+            <div className="h-full mt-5 flex overflow-y-auto flex-col flex-grow ">
                 <UserGuard>
                     <Outlet />
                 </UserGuard>
