6967492f537b98ac1fd6b8ec2ec7756bfbb11cef
Brian Leach Christensen <30732216+blcoyote@users.noreply.github.com>
2024-07-19 16:10:01 +0200

    Update layout.tsx

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -4,7 +4,7 @@ import { UserGuard } from "../guards/user-guard";
 
 export const Layout = () => {
     return (
-        <div className="flex flex-col p-2 min-h-dvh bg-stone-200 ">
+        <div className="flex flex-col p-2 min-h-dvh bg-base-300 ">
             <Navbar />
             <div className="h-full flex overflow-y-auto flex-col flex-grow ">
                 <UserGuard>
