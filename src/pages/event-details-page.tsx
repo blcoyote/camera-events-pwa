b6f6eceac6c574deb476d811213a4f60f505ee83
Brian Leach Christensen <30732216+blcoyote@users.noreply.github.com>
2024-07-19 19:55:17 +0200

    temp fix to redux issue

--- src/pages/event-details-page.tsx
+++ src/pages/event-details-page.tsx
@@ -5,10 +5,10 @@ import {
     useNavigate,
 } from "react-router-dom";
 import { IconArrowBackUp } from "@tabler/icons-react";
-import { useEventSnapshot } from "../hooks/use-event";
+import { useEventDetails, useEventSnapshot } from "../hooks/use-event";
 import { EventContainer } from "../components/event-card/event-card";
 import { motion } from "framer-motion";
-import { useGetCameraEventDetailsQuery } from "../services/camera-api";
+//import { useGetCameraEventDetailsQuery } from "../services/camera-api";
 
 export const Component = () => {
     const { id } = useParams();
@@ -26,9 +26,16 @@ export const Component = () => {
     const {
         data: eventData,
         isSuccess,
-        isLoading,
-        isError,
-    } = useGetCameraEventDetailsQuery(id ?? "", { skip: !id });
+        loading: isLoading,
+        error: isError,
+    } = useEventDetails(id);
+
+    // const {
+    //     data: eventData,
+    //     isSuccess,
+    //     isLoading,
+    //     isError,
+    // } = useGetCameraEventDetailsQuery(id ?? "", { skip: !id });
 
     const { loading: snapshotLoading, data: snapshotData } =
         useEventSnapshot(id);
@@ -50,7 +57,7 @@ export const Component = () => {
                     <EventContainer
                         event={eventData}
                         isLoading={isLoading}
-                        isError={isError}
+                        isError={isError !== null}
                         isSuccess={isSuccess}
                         isSnapshotLoading={snapshotLoading}
                         snapshotData={snapshotData}
--- src/pages/events-list-page.tsx
+++ src/pages/events-list-page.tsx
@@ -1,10 +1,12 @@
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 import { EventListCard } from "../components/event-list-card";
 import { EventListCardLoader } from "../components/event-list-card/event-list-card-loader";
-import { useGetCameraEventsQuery } from "../services/camera-api";
+//import { useGetCameraEventsQuery } from "../services/camera-api";
+import { useEventList } from "../hooks/use-events-list";
 
 export const Component = () => {
-    const { data, isLoading } = useGetCameraEventsQuery(10);
+    //const { data, isLoading } = useGetCameraEventsQuery(10);
+    const { data, loading: isLoading } = useEventList();
 
     return (
         <div className="grid content-center pt-5 lg:grid-cols-2 gap-4 w-max-dvw">
