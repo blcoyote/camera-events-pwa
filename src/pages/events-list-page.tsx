0c8d41363915ad923fd2dfbf32e7e0de94111740
Brian Leach Christensen <30732216+blcoyote@users.noreply.github.com>
2024-10-21 14:57:02 +0200

    add pull to refetch event list

--- src/pages/events-list-page.tsx
+++ src/pages/events-list-page.tsx
@@ -5,14 +5,25 @@ import { useGetCameraEventsQuery } from "../services/camera-api";
 import useSettings from "../hooks/use-settings";
 import { useIdToken } from "react-firebase-hooks/auth";
 import { auth } from "../config/firebase";
+import PullToRefresh from "pulltorefreshjs";
+import { useEffect } from "react";
 
 export const Component = () => {
     const { eventLimit } = useSettings();
     const [user] = useIdToken(auth);
     const token = user?.getIdToken();
-    const { data, isLoading } = useGetCameraEventsQuery(eventLimit, {
+    const { data, isLoading, refetch } = useGetCameraEventsQuery(eventLimit, {
         skip: !token,
     });
+
+    useEffect(() => {
+        PullToRefresh.init({
+            mainElement: "body",
+            onRefresh() {
+                refetch();
+            },
+        });
+    }, []);
 
     return (
         <div className="grid content-center pt-5 lg:grid-cols-2 gap-4 w-max-dvw">
