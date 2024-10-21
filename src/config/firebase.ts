3b1a3a1056045411806809495f5eb9de10835e0b
Brian Leach Christensen <30732216+blcoyote@users.noreply.github.com>
2024-10-21 11:02:01 +0200

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,50 +1,53 @@
 import { type FirebaseOptions, initializeApp } from "firebase/app";
-import { GoogleAuthProvider,  getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import {
-	type MessagePayload,
-	getToken,
-	onMessage,
-	getMessaging,
+    type MessagePayload,
+    getToken,
+    onMessage,
+    getMessaging,
 } from "firebase/messaging";
 
 export const firebaseConfig: FirebaseOptions = {
-	apiKey: import.meta.env.VITE_apiKey,
-	authDomain: import.meta.env.VITE_authDomain,
-	projectId: import.meta.env.VITE_projectId,
-	storageBucket: import.meta.env.VITE_storageBucket,
-	messagingSenderId: import.meta.env.VITE_messageSenderId,
-	appId: import.meta.env.VITE_appId,
-	measurementId: import.meta.env.VITE_measurementId,
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messageSenderId,
+    appId: import.meta.env.VITE_appId,
+    measurementId: import.meta.env.VITE_measurementId,
 };
 
-export const app = initializeApp(firebaseConfig, 'CameraEvents');
+export const app = initializeApp(firebaseConfig);
 
-export const auth= getAuth(app)
+export const auth = getAuth(app);
 
 export const googleProvider = new GoogleAuthProvider();
 
 export const messaging = getMessaging(app);
 
 export const getMessageToken = async (
-	setTokenFound: React.Dispatch<React.SetStateAction<string | undefined>>,
+    setTokenFound: React.Dispatch<React.SetStateAction<string | undefined>>,
 ) => {
-	try {
-		const currentToken = await getToken(messaging, { vapidKey: import.meta.env.VITE_MESSAGINGKEY });
-		if (currentToken) {
-			//"current fcm token for client: ", currentToken
-			setTokenFound(currentToken);
-		} else {
-			//'No registration token available. Request permission to generate one.'
-			setTokenFound(undefined);
-		}
-	} catch (err) {
-		console.log("An error occurred while retrieving token. ", err);
-	}
+    try {
+        const currentToken = await getToken(messaging, {
+            vapidKey: import.meta.env.VITE_MESSAGINGKEY,
+        });
+        if (currentToken) {
+            //"current fcm token for client: ", currentToken
+            setTokenFound(currentToken);
+        } else {
+            //'No registration token available. Request permission to generate one.'
+            setTokenFound(undefined);
+        }
+    } catch (err) {
+        console.log("An error occurred while retrieving token. ", err);
+    }
 };
 
 export const onMessageListener = (): Promise<MessagePayload> =>
-	new Promise((resolve) => {
-		onMessage(messaging, (payload) => {
-			resolve(payload);
-		});
-	});
+    new Promise((resolve) => {
+        onMessage(messaging, (payload) => {
+            resolve(payload);
+        });
+    });
+
