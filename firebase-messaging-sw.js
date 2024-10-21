9068a976609eef32f793476d357e0822c75be894
Brian Leach Christensen <30732216+blcoyote@users.noreply.github.com>
2024-10-21 14:07:15 +0200

    upgrade packages

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 importScripts(
-    "https://www.gstatic.com/firebasejs/10.12.4/firebase-app-compat.js"
+    "https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js"
 );
 importScripts(
-    "https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging-compat.js"
+    "https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js"
 );
 
 // Initialize the Firebase app in the service worker by passing the generated config
